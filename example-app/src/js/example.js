c1ccc0ec656b09f2d32bb55259167c7ee260e20c
Rick Kock <rick@varremarkt21.nl>
2024-09-26 08:14:36 +0200

    Initial Release

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { NativeActionSheet } from '@capacitor&#x2F;native-action-sheet';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    NativeActionSheet.echo({ value: inputValue })
-}
